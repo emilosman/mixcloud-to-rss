c0d6a900cfb0337436801da958196f2b2c348db4
Emil Osmanbegović <emilosmanbegovic@gmail.com>
2023-01-15 19:10:55 +0100

    add basic form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import styles from '../styles/Home.module.css'
+
 export default function Home() {
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const username = event.target.username.value;
+    console.log(username)
+    window.location.href = '/feeds/' + username;
+  }
+
   return (
-    'hello world'
+    <>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label htmlFor="username">Enter the Mixcloud username</label>
+        <input type="text" id="username" name="username" required />
+        <button type="submit">Submit</button>
+      </form>
+    </>
   )
 }
